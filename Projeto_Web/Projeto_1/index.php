5abecce498b5ee6f98d2838b470beada53568ccd
roudry2 <roudry2@hotmail.com>
2016-06-16 10:32:22 +0100

    projeto

---
+++ Projeto_Web/Projeto_1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+	
+	
+	<div class="head">
+		
+		<img src="#" alt="">
+
+		<ul>
+			<li><label for="User">Username: </label><input id="User" type="text"></li>
+			<li><label for="Pass">Password: </label><input id="Pass" type="text"></li>
+			<li><button>Login</button></li>
+			<li><button>Registar</button></li>
+		</ul>
+			
+			
+			
+			
+
+	</div>
+
+
+
+
+
+</body>
+</html>
